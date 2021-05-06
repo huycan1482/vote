974d46ec00c0589d4918d838a1efe37fd766bfaf
huycan1482 <53845397+huycan1482@users.noreply.github.com>
2021-05-06 15:54:26 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+    $(window).width( function () {
+        if ($(this).width() > 780) {
+            $('.slider').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                prevArrow:'<i class="fas fa-chevron-left slick-prev"></i>',
+                nextArrow:'<i class="fas fa-chevron-right slick-next"></i>',
+                autoplay: true,
+                autoplaySpeed: 5000,
+            });
+
+            $('.slider2').slick({
+                slidesToShow: 5,
+                slidesToScroll: 2,
+                prevArrow:'<i class="fas fa-chevron-left slick-prev"></i>',
+                nextArrow:'<i class="fas fa-chevron-right slick-next"></i>',
+                autoplay: true,
+                autoplaySpeed: 5000,
+            });
+            
+        } else {
+            $('.slider').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow:"<i class='fas fa-chevron-left slick-prev'></i>",
+                nextArrow:"<i class='fas fa-chevron-right slick-next'></i>",
+                autoplay: true,
+                autoplaySpeed: 5000,
+            });
+
+            $('.slider2').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow:"<i class='fas fa-chevron-left slick-prev'></i>",
+                nextArrow:"<i class='fas fa-chevron-right slick-next'></i>",
+                autoplay: true,
+                autoplaySpeed: 5000,
+            });
+        }
+    });
+
+    // $(window).resize( function () {
+    //     console.log('?')
+    //     if ($(this).width() > 780) {
+    //         $('.slider').slick({
+    //             slidesToShow: 3,
+    //             slidesToScroll: 1,
+    //             // prevArrow:'<i class="fas fa-chevron-left slick-prev"></i>',
+    //             // nextArrow:'<i class="fas fa-chevron-right slick-next"></i>',
+    //             autoplay: true,
+    //             autoplaySpeed: 1000,
+    //         });
+    //     } else {
+    //         $('.slider').slick({
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1,
+    //             // prevArrow:"<i class='fas fa-chevron-left slick-prev'></i>",
+    //             // nextArrow:"<i class='fas fa-chevron-right slick-next'></i>",
+    //             autoplay: true,
+    //             autoplaySpeed: 1000,
+    //         });
+    //     }
+    // });
+});
